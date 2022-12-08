ba6197b7e56a04ee321968101ede1ea2dd6b358c
Krima <06sebastian.nh@gmail.com>
2022-12-08 20:55:19 +0100

    commit

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -2,3 +2,5 @@ var v = document.querySelector(".wrapper");
 document.addEventListener("DOMContentLoaded", function(){
     v.classList.add('loaded');
 }); 
+
+document.querySelector(".year").innerHTML = new Date().getFullYear();
